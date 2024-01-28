41f2a67843853b2d3e840e009fe3525f0ae17eae
LyhengPHA <157772498+LyhengPHA@users.noreply.github.com>
2024-01-28 15:28:53 +0700

    Update process-string.py

--- process-string.py
+++ process-string.py
@@ -21,4 +21,30 @@
 alphabet = "abcdefghijklmnopqrstuvwxyz"
 user_range = input("Enter a range of letters (e.g., a-z): ")
 
+# Application 1
+user_input = input("Enter any words: ")
+user_input1 = list(user_input)
+user_input2 = list(user_input)
+
+user_input1_2 = [None]*(len(user_input1)+len(user_input2))
+user_input1_2[::2] = user_input1
+user_input1_2[1::2] = user_input2
+
+user_str = ''. join(user_input1_2)
+print(user_str)
+
+
+
+# Application 2
+alpha = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+user_input = input("Enter a range of letters (e.g., a-z):")
+start, end = user_input.split("-")
+start_letter = alpha.index(start)
+end_letter = alpha.index(end)
+result_string = alpha[start_letter:end_letter+1]
+
+print(result_string)
+
+
 
